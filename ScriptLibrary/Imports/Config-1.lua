91ffa4d32d2fc59338248f5bb18edcbe49712c93
Ghost-X Collective <108462714+ProxColdAirPublishing@users.noreply.github.com>
2022-08-13 15:32:26 +1000

    Update Config-1.lua

--- ScriptLibrary/Imports/Config-1.lua
+++ ScriptLibrary/Imports/Config-1.lua
@@ -1,1 +1,80 @@
+window:CreateListing(
+	"Owl Hub", 
+	"Owl Hub supports many different games and also includes a universal script. Owl hub is a trusted script and is reliable",
+	true,
+	"Owl Hub Arsenal Assassin Big PaintBall Knife Ability Test | KAT keyless Universal Synapse Krnl",
+function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+end)
 
+window:CreateListing(
+	"Dark Hub", 
+	"Dark Hub is a trusted and reliable script that thousands of people use. Darkhub supports many games whilst providing premuim quality scripts. Join their discord for the key.",
+	true, --Starred Flase\True
+	"Dark Hub Arsenal Midnight Racing Bas Business Big Paintball Strucid Project Bronze Phantom Forces Project Lazarus Rasied By Floppa Weaponry Nico Nexbots Knife Ability Test | KAT Rainbow Friends Universal",
+function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/Init", true))()
+end)
+
+window:CreateListing(
+	"Vape V4 | BedWars", 
+	"Vape V4 is a replica of the Vape hack for minecraft but made for roblox bedwars, this script is reliable and trusted to deliver a premium experience when using this feature packed script for roblox bedwars.",
+	true, --Starred Flase\True
+	"VapeV4 Vap, BedWars Free Keyless",
+function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end)
+
+window:CreateListing(
+	"Breaking Point", 
+	"This breaking point script provides: Silent Aim/Aimbot, Kill all and more!",
+	false, --Starred Flase\True
+	"Breaking Point Free Keyless",
+function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/fFmJFepC"))()
+end)
+
+window:CreateListing(
+	"Wally | Funky Friday", 
+	"This Funky Friday script is made by a reputable developer wally-rblx. This script is trusted and reliable to give the user a premium experience",
+	false, --Starred Flase\True
+	"Funky Friday",
+function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/funky-friday-autoplay/main/main.lua",true))()
+end)
+
+window:CreateListing(
+	"Infinite Yield | Admin Commands", 
+	"infiniteyield is a trusted and relaible script that has been around forever! infiniteyield will deliver the *Best* experience for any admin command scripts!",
+	true, --Starred Flase\True
+	"Keyless Free Admin Commands Infinite Yield",
+function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+window:CreateListing(
+	"CumlinHub | *35+ Games!*", 
+	"CumlinHub is a large script hub that supports 35+ games from Pet Sim X to Grand Piece Online!",
+	false, --Starred Flase\True
+	"CumlinHub script hub PET SIMULATOR XSHINDO LIFEYBAANIME FIGHTERS SIMULATORBLOX FRUITSADOPT MEBEE SWARM SIMULATORGRAND PIECE ONLINEABAASTDJAILBREAKSABER SIMULATORSPEEDMAN SIMULATORBEDWARSKING LEGACYMAD CITYDA HOODARSENALSTRONGEST PUNCHANIME TAPPERSPET LEGENDSANIME DIMENSIONSKATLEGENDS OF SPEEDA UNIVERSAL TIMEBUBBLE GUM SIMULATORRUSSIA RPTRUE PIECEMURDER MYSTERY 2ATSANIME CLICKERSWEAPON FIGHTING SIMULATORCLICKER SIMULATORDEMONFALLABXAPS3008SONIC SPEED SIMULATORMINING SIMULATOR 2ANIME ADVENTURES",
+function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/cumlin21/CumlinHub/main/1.txt"))()
+end)
+
+window:CreateListing(
+	"Zeus Hub | Arsenal & More", 
+	"Zues hub was made with the orion UI Library. Zues hub is a free script hub that supports 3 main games!",
+	false, --Starred Flase\True
+	"Zeus Hub Keyless Free Admin Commands Infinite Yield",
+function()
+	loadstring(game:HttpGet(("https://pastebin.com/raw/QM211VHT"),true))()
+end)
+
+window:CreateListing(
+	"Zephyr X | *20 Games*", 
+	"Zephyr X is a script hub that supports 20 games like Arsenal, MM2, Pet Sim X and More!",
+	false, --Starred Flase\True
+	"Zephyr X Script hub ARSENALPHANTOM FORCESZOMBIE RUSHRABBIT SIMULATOR 2WEAPON FIGHTING SIMULATORMURDER MYSTERY 2MALL TYCOONBLOX FRUITSBROKEN BONESLOOMIAN LEGACYISLAND SURVIVALRETAIL TYCOON 2TIMBERWORK AT A PIZZA PLACEZOMBIE LAZARUSDRIVING SIMULATORPET SIM XPOP IT TRADINGCREATURES OF SONARIAMAD CITY",
+function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ECLIPSEXHUB/ECLIPSE-X/main/ECLIPSE%20X.txt", true))()
+end)
